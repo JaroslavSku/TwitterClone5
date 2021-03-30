b1cdc1266e793dc2ad2037e21f8f67bfbe78e113
skudrnaj <jaroslavskudrna@gmail.com>
2021-03-30 21:52:20 +0200

    first commit Twitter reference project

---
+++ Twitter/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Twitter
+//
+//  Created by MacBook Pro on 13.06.16.
+//  Copyright © 2016 Akhmed Idigov. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Twitter/UsersCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsersCell.swift
+//  Twitter
+//
+//  Created by MacBook Pro on 14.06.16.
+//  Copyright © 2016 Akhmed Idigov. All rights reserved.
+//
+
+import UIKit
+
+class UsersCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
